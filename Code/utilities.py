35d472c678e73fbc78d3e209f4422193272f0c8d
mehmetcandfx <mehmetcandfx@gmail.com>
2023-05-09 13:05:43 +0200

    .

--- Code/utilities.py
+++ Code/utilities.py
@@ -5,16 +5,8 @@ import os
 import utilities as u
 import eurostat
 
-def plustwo(n):
-    out = n + 2
-    return out
 
-
-def falldist(t,g=9.81):
-    d = 0.5 * g * t**2
-    return d
-
-def wageget(db):
+def cat_describer(db):
     df=pd.DataFrame()
     for ds in db:
         dftemp = pd.DataFrame({
