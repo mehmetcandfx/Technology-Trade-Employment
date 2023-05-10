cfa266f13676ff753d525712a750e95f625d56f0
mehmetcandfx <mehmetcandfx@gmail.com>
2023-05-10 11:10:12 +0200

    .

--- Code/Utilities/wages.py
+++ Code/Utilities/wages.py
@@ -6,15 +6,22 @@ import eurostat
 
 
 def cat_describer(db):
+    
+    try:
+        yyyy=2000+ int(ds[-5:-3])
+    except:
+        yyyy=2000+ int(ds[-8:-6])
+    
     df=pd.DataFrame()
     for ds in db:
         dftemp = pd.DataFrame({
             'Dataset': [ds for i in eurostat.get_pars(ds)],
-            'Year': [ 2000+int(ds[-5:-3]) for i in eurostat.get_pars(ds)], 
+            'Year': [ yyyy for i in eurostat.get_pars(ds)], 
             'Category' : eurostat.get_pars(ds),
             '#ofUniques': [len(eurostat.get_par_values(ds, i)) for i in  eurostat.get_pars(ds)],
             'Uniques': [eurostat.get_par_values(ds, i) for i in  eurostat.get_pars(ds)]
             })
         df = pd.concat([df, dftemp], ignore_index=True)
-        df.set_index(['Dataset','Year'])
+        
+    df.set_index(['Dataset','Year'])
     return df
